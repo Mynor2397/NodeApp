86be7a8734fa9354ded71a13bdd63d4913bc111d
Mynor2397 <Mynor2397>
2019-10-26 00:13:48 -0600

    El servidor

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use(express.static(__dirname + '/public/'));
 
-app.listen('3000', function() {
+app.listen('3000H', function() {
   console.log('Servidor web escuchando en el puerto 3000');
 });
