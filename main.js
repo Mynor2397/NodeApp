7a84679f861071a8da786de6763475b833fe1204
Mynor2397 <Mynor2397>
2019-11-09 07:26:31 -0600

    Cambio de puerto

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use(express.static(__dirname + '/public/'));
 
-app.listen('2020', function() {
+app.listen('3030', function() {
   console.log('Servidor web escuchando en el puerto 3000');
 })
